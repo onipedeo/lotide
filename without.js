18d2bbd80ce7ee720fb5a6afa85d1d9837dedf3c
Tobi Onipede <onipede89@gmail.com>
2023-05-19 23:27:18 +0100

    This function takes in a source array and a itemsToRemove array and returns a new array with only those elements from source that are not present in the itemsToRemove array

--- without.js
+++ without.js
@@ -45,8 +45,9 @@ const without = function(source, itemsToRemove) {
 };
 
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
+
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 
