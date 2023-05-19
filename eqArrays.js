2c97a1df1aee1c04c11eb07806c2166f1a585d41
Tobi Onipede <onipede89@gmail.com>
2023-05-19 19:15:34 +0100

    logic correction again

--- eqArrays.js
+++ eqArrays.js
@@ -11,7 +11,7 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
 
-  for (let i = 0; i < arr1.length - 1; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
