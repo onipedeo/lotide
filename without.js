974e2f9190a5848bbe2114194e43d71723557c47
Tobi Onipede <onipede89@gmail.com>
2023-05-21 00:53:35 +0100

    tested the without function with another use case

--- without.js
+++ without.js
@@ -48,6 +48,6 @@ const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(words, without(words, ["lighthouse"]));
 
 
