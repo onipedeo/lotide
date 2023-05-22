1b22a9175040b52fd6ce4a3cfd0bb6c261a13418
Tobi Onipede <onipede89@gmail.com>
2023-05-22 22:32:02 +0100

    iterated over the string successfully

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`😡😡😡 Assertion failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let result = {};
+  for (const letters of str) {
+    console.log(letters);
+    if (result[letters]) {
+      result[letters] += 1;
+    } else {
+      result[letters] = 1;
+    }
+  }
+  return result;
+};
+const nameOne = "Oluwatobi";
+
+assertEqual(countLetters(nameOne), 2);
