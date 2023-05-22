946bc9630bb512f12cf61971fcd1eec97c67494f
Tobi Onipede <onipede89@gmail.com>
2023-05-23 00:39:01 +0100

    re-engineered using a C-style for loop

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,45 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr3, arr4) {
+  if (eqArrays(arr3, arr4)) {
+    console.log(`The two arrays are the same: ${arr3} === ${arr4}.`);
+  } else {
+    console.log(`The two arrays are not the same: ${arr3} !== ${arr4}.`);
+  }
+};
+
+
 const letterPositions = function(sentence) {
   const results = {};
 
+  for (let i = 0; i < sentence.length; i++) {
+    const key = sentence[i];
+
+    //ommitting the spaces inbetween the strings
+    if (key !== " ") {
+      //if the key is not in the object, create an array
+      if (!results[key]) {
+        results[key] = [];
+      }
+      //If key is in object, then push the index to the array initially created
+      results[key].push(i);
+    }
+  }
+
   return results;
-}
+};
+
+//Test case
+const result = letterPositions("lighthouse in the house");
