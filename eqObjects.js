5a9d7fa6287e92637a158dd933acdc90b68c5071
Tobi Onipede <onipede89@gmail.com>
2023-05-23 18:19:58 +0100

    function definition

---
+++ eqObjects.js
@@ -0,0 +1,9 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`😡😡😡 Assertion failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
