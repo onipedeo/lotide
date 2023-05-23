880a4ae6119ea678997352a6d51ade3ac168b16f
Tobi Onipede <onipede89@gmail.com>
2023-05-23 18:11:04 +0100

    function that takes in an object and a value, scans the object and returns the first key which contains the given value. If no key with that given value is found, then it returns undefined.

---
+++ findKeyByValue.js
@@ -0,0 +1,39 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`😡😡😡 Assertion failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let firstKey = undefined;
+  for (const elements in obj) {
+    // console.log(elements);
+    if (obj[elements] === value) {
+      firstKey = elements;
+    }
+  }
+  return firstKey;
+};
+
+let myObj = {
+  name: "Tobi",
+  class: "winner",
+  age: 7,
+  hobby: "gaming",
+  favCar: "sportage"
+};
+const val = 'sportage';
+
+console.log(findKeyByValue(myObj, val));
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
