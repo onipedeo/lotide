e89bab3a24a2b0d3ac746c32ec45647d94c16c58
Tobi Onipede <onipede89@gmail.com>
2023-05-23 18:26:11 +0100

    function currently return undefined

--- eqObjects.js
+++ eqObjects.js
@@ -7,3 +7,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// returns true if both objects have identical keys with identical values
+// otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+let case1 = eqObjects(shirtObject, anotherShirtObject); //=> true
+
+const longSleeveShitObject = { size: "medium", color: "red", sleeveLength: "long" };
+let case2 = eqObjects(shirtObject, longSleeveShitObject); //=> true
+
+
+console.log(case1);
+console.log(case2);
