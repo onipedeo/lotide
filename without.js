eb9f0caa45b59064a670810ab1d46e98c4f15a70
Tobi Onipede <onipede89@gmail.com>
2023-06-10 04:37:35 +0100

    cleaned up without file and imported the needed module

--- without.js
+++ without.js
@@ -1,46 +1,20 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr3, arr4) {
-  if (eqArrays(arr3, arr4)) {
-    console.log(`The two arrays are the same: ${arr3} === ${arr4}.`);
-  } else {
-    console.log(`The two arrays are not the same: ${arr3} !== ${arr4}.`);
-  }
-};
-const arr = [1, 2, 3, 9];
-const array = [1, 2, 3, 7];
-assertArraysEqual(arr, array);
+const assertArraysEqual = require("./assertArraysEqual");
 
 const without = function(source, itemsToRemove) {
   const result = [];
-
   for (let i = 0; i < source.length; i++) {
     let shouldRemove = false;
-
     for (let j = 0; j < itemsToRemove.length; j++) {
       if (source[i] === itemsToRemove[j]) {
         shouldRemove = true;
         break;
       }
     }
-
     if (!shouldRemove) {
       result.push(source[i]);
     }
   }
-
   return result;
 };
 
@@ -49,3 +23,5 @@ console.log(without(words, ["lighthouse"])); // no need to capture return value 
 // Make sure the original array was not altered by the without function
 
 assertArraysEqual(words, without(words, ["lighthouse"]));
+
+module.exports = without;
